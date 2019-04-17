7edc4286da811fd619454317cc0c8168f6f2e6cf
Tim Haynes <thaynes19@cornellcollege.edu>
2019-04-16 21:22:58 -0500

    Improve Button Press
    Change so only the button which was pressed will change colors, improving visability

--- src/Capital.js
+++ src/Capital.js
@@ -3,7 +3,7 @@ import React from 'react'
 class Capital extends React.Component{
   constructor(){
     super()
-    this.state = {answered: null}
+    this.state = {answered: null, clicked: null}
     this.handleClickTrue = this.handleClickTrue.bind(this)
     this.handleClickFalse = this.handleClickFalse.bind(this)
   }
@@ -12,6 +12,7 @@ class Capital extends React.Component{
     var trueFalse = this.props.tf
     if(this.state.answered === null){
       trueFalse ? this.setState({answered: true}) : this.setState({answered: false})
+      this.setState({clicked: true})
     }
   }
 
@@ -19,15 +20,27 @@ class Capital extends React.Component{
     var trueFalse = this.props.tf
     if(this.state.answered === null){
       trueFalse ? this.setState({answered: false}) : this.setState({answered: true})
+      this.setState({clicked: false})
     }
   }
 
-  colorChange(){
+  colorChange1(){
     let col = "#0000ff"
-    if(this.state.answered === true){
+    if((this.state.answered === true) && (this.state.clicked === true)){
       col = "#00ff00"
     }
-    if(this.state.answered === false){
+    if((this.state.answered === false) && (this.state.clicked === true)){
+      col = "#ff0000"
+    }
+    return col
+  }
+
+  colorChange2(){
+    let col = "#0000ff"
+    if((this.state.answered === true) && (this.state.clicked === false)){
+      col = "#00ff00"
+    }
+    if((this.state.answered === false) && (this.state.clicked === false)){
       col = "#ff0000"
     }
     return col
@@ -38,11 +51,11 @@ class Capital extends React.Component{
       <div>
         <p className = "country">{this.props.capital} --> {this.props.country}</p>
         <button style =
-          {{backgroundColor : this.colorChange()}}
+          {{backgroundColor : this.colorChange1()}}
           className = "button" onClick={this.handleClickTrue}>True</button>
-        <span>  </span>
+          <span>  </span>
         <button style =
-        {{backgroundColor : this.colorChange()}}
+        {{backgroundColor : this.colorChange2()}}
           className = "button" onClick={this.handleClickFalse}>False</button>
         <hr  style = {{width: "200px", marginLeft:"0px"}}/>
       </div>
